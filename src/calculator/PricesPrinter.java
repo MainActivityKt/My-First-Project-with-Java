d84dfcdc8c1bf7854eb4b7030f34452b9ecea117
Safi <safiullahkakar2020@outlook.com>
2024-09-10 00:12:47 +0430

    Added stage 1 solution code

---
+++ src/calculator/PricesPrinter.java
@@ -0,0 +1,17 @@
+package calculator;
+
+class Main {
+    public static void main(String[] args) {
+        StringBuilder text = new StringBuilder();
+
+        text.append("Prices:\n");
+        text.append("Bubblegum: $2\n");
+        text.append("Toffee: $0.2\n");
+        text.append("Ice cream: $5\n");
+        text.append("Milk chocolate: $4\n");
+        text.append("Doughnut: $2.5\n");
+        text.append("Pancake: $3.2\n");
+
+        System.out.println(text);
+    }
+}
